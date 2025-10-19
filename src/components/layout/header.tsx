d8286c39dd727fefb7c032505c4eee01b982162f
Keane Putra Setiawan <keanesetiawan.21@gmail.com>
2025-10-19 16:58:59 +0900

    style: increase font size

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header>
       <nav className="container mx-auto flex items-center justify-between px-4 py-4">
-        <Link href="/" className="text-xl font-bold">
+        <Link href="/" className="text-2xl font-bold">
           Recipify
         </Link>
         <div className="flex gap-4 text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 rounded-lg text-sm px-5 py-2.5 text-center items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2 font-bold">
